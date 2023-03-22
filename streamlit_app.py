50234947e2c834c90ff9b7311cb2665b8f6bdc41
Hassan Waqar <54843455+apophis-web@users.noreply.github.com>
2023-03-22 21:29:00 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,25 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.set_page_config(page_title='Bar Chart with Positive Negative Reviews')
+st.set_page_config(page_title='Bar Chart with Two Entities')
 
+# Define data
 data = pd.DataFrame({
     'entity': ['positive', 'negative'],
     'value': [10, 20]
 })
 
 bar_chart = alt.Chart(data).mark_bar().encode(
-    x='Label',
-    y='Count'
+    x='label',
+    y='value'
 ).properties(
     width=alt.Step(80)
 )
 
+# Render streamlit app
 def app():
-    st.title('Bar Chart with Positive Negative Reviews')
-    st.write('This app creates a bar chart with positive negative reviews')
+    st.title('Bar Chart with Two Entities')
+    st.write('This app creates a bar chart with two entities')
     st.altair_chart(bar_chart, use_container_width=True)
 
 if __name__ == '__main__':
